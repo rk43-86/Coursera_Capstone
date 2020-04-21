019e9a7b557ff8a64f823ceef80c9f8da1ea8f41
rk43-86 <64082705+rk43-86@users.noreply.github.com>
2020-04-21 18:58:26 +0530

    Add files via upload

---
+++ Capstone-New.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project Github Trial
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
